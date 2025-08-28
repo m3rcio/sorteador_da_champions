e7e62241297e0b544b0f6befe016db7b7c340ba8
Mércio <mercio20a@gmail.com>
2025-08-29 00:25:47 +0100

    instancia de um clube

---
+++ sorteador.ts
@@ -0,0 +1,10 @@
+import { Clube, Pote } from './classes';
+
+
+const benfica = new Clube("1", "Benfica", "https://img.uefa.com/imgml/TP/teams/logos/32x32/50147.png", 1, "Portugal", []);
+const pote1 = new Pote(1, []);
+
+
+function organizarPotes(){
+    // return potes
+}
