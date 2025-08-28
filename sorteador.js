41e3b12e5bdae79ae3b86e7b31af0c10a36edcbe
Mércio <mercio20a@gmail.com>
2025-08-29 00:07:34 +0100

    criação das classes clube e pote

--- sorteador.js
+++ sorteador.js
@@ -1,13 +1,4 @@
-clube={
-  id: string,                
-  nome: string,
-  logo: string,
-  pote: number,
-  pais: string,
-  jogos: [
-    { adversarioId: string, local: "casa" | "fora" }
-  ]
-}
+
 
 function teste(){
     alert(" teste ");
