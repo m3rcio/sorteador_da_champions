a3578faf6ef0e45b13e6471902d859bdb5b45357
Mércio <mercio20a@gmail.com>
2025-08-28 23:54:46 +0100

    arquivos criados

---
+++ sorteador.js
@@ -0,0 +1,14 @@
+clube={
+  id: string,                
+  nome: string,
+  logo: string,
+  pote: number,
+  pais: string,
+  jogos: [
+    { adversarioId: string, local: "casa" | "fora" }
+  ]
+}
+
+function teste(){
+    alert(" teste ");
+}
